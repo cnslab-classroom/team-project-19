c99fe7ec9ec1d058a404181b81dc21900990d77a
CHEON JIYEON <milk0618@kw.ac.kr>
2024-11-22 15:05:53 +0900

    뭔가

--- app/src/main/java/com/example/oeg/Etc/MYAccessibilityService.java
+++ app/src/main/java/com/example/oeg/Etc/MYAccessibilityService.java
@@ -3,13 +3,12 @@ package com.example.oeg.Etc;
 import android.accessibilityservice.AccessibilityService;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
-import android.widget.Toast;
 
 import java.util.List;
 
 public class MYAccessibilityService extends AccessibilityService {
 
-    public static String selectedText = ""; // 전역 변수로 선택된 텍스트 저장
+    private static String currentSelectedText = "";
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -17,15 +16,20 @@ public class MYAccessibilityService extends AccessibilityService {
             // 선택된 텍스트 가져오기
             List<CharSequence> textList = event.getText();
             if (textList != null && !textList.isEmpty()) {
-                selectedText = textList.get(0).toString();
-                Log.d("SelectedText", "드래그한 텍스트: " + selectedText);
+                currentSelectedText = textList.get(0).toString();
+                Log.d("SelectedText", "드래그한 내용: " + currentSelectedText);
             }
         }
+
     }
 
 
     @Override
     public void onInterrupt() {
         // 서비스가 인터럽트되었을 때 처리하는 부분
+    }
+
+    public static String getCurrentSelectedText() {
+        return currentSelectedText;
     }
 }
