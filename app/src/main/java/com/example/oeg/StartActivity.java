cf04421052385cf227d0f8bc1f16da54d468cb39
Lee Chae Eun <chaeeun8837@kw.ac.kr>
2024-12-14 21:44:52 +0900

    Add files via upload

--- app/src/main/java/com/example/oeg/StartActivity.java
+++ app/src/main/java/com/example/oeg/StartActivity.java
@@ -6,36 +6,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 
 public class StartActivity extends AppCompatActivity{
+
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        SharedPreferences prefs = getSharedPreferences("Prefs", MODE_PRIVATE);
-        boolean isFirstRun = prefs.getBoolean("isFirstRun", true);
+        // 버튼 찾아오기
+        imageView = findViewById(R.id.button_image);
 
-        if (isFirstRun) {
-            Intent intent = new Intent(this, IntroActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            setContentView(R.layout.activity_main);
 
-            // 버튼 찾아오기
-            Button button = findViewById(R.id.start_button);
+        // 버튼 클릭 시 MainActivity로 이동
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.drawable.start_button_pressed);
 
-            // 버튼 클릭 시 MainActivity로 이동
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // MainActivity로 이동
-                    Intent intent = new Intent(StartActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish(); // StartActivity 종료
-                }
-            });
-        }
+                // 몇 초 기다린 후 MainActivity로 이동
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish(); // StartActivity 종료
+                    }
+                }, 200);
+            }
+        });
     }
 }
