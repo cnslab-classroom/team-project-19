52f382ca6a87dd5f9cfe74ecaeecf716c82777c9
CHEON JIYEON <milk0618@kw.ac.kr>
2024-12-15 09:45:32 +0900

    Add files via upload

--- app/src/main/java/com/example/oeg/StartActivity.java
+++ app/src/main/java/com/example/oeg/StartActivity.java
@@ -23,26 +23,35 @@ public class StartActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 버튼 찾아오기
-        imageView = findViewById(R.id.button_image);
+        SharedPreferences prefs = getSharedPreferences("Prefs", MODE_PRIVATE);
+        boolean isFirstRun = prefs.getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            Intent intent = new Intent(this, IntroActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // 버튼 찾아오기
+            imageView = findViewById(R.id.button_image);
 
 
-        // 버튼 클릭 시 MainActivity로 이동
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageView.setImageResource(R.drawable.start_button_pressed);
+            // 버튼 클릭 시 MainActivity로 이동
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    imageView.setImageResource(R.drawable.start_button_pressed);
 
-                // 몇 초 기다린 후 MainActivity로 이동
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(StartActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish(); // StartActivity 종료
-                    }
-                }, 200);
-            }
-        });
+                    // 몇 초 기다린 후 MainActivity로 이동
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish(); // StartActivity 종료
+                        }
+                    }, 200);
+                }
+            });
+        }
     }
 }
